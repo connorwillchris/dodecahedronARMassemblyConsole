40fc09047b86d6f2f7f6b8fd1cd5536f1d1b85a2
treeman0013 <connorwillchris@gmail.com>
2026-07-12 12:39:49 -0600

    updates...

--- test/main.s
+++ test/main.s
@@ -1,53 +1,43 @@
-// boilerplate main file
-
-.section ".text.boot"
 .global _start
-.global _bss_start
-.global _bss_size
 
+.equ GPIO_BASE, 0x3f200000
+.equ GPFSEL2, 0x8
+.equ GPIO_21_OUTPUT, 0x8 // 1 << 3
+.equ GPFSET0, 0x1c
+.equ GPFCLR0, 0x28
+.equ GPIOVAL, 0x200000 // 1 << 21
+
+
+//.text
 _start:
-    // STARTUP SECTION - PARK OTHER CORES
-    mrs x1, MPIDR_EL1
-    and x1, x1, #0xff
-    cbz x1, onMainCore
-1: // the secondary core loop
-    wfe
-    b 1b // the secondary core loop
-    mrs x1, MPIDR_EL1 // get core id
-    and x1, x1, #0x03
-    mov x5, #0x8
-    mul x2, x1, x5
-    add x2, x2, #0xe0
-    ldr x3, [x2]
-    dmb ish
-    cbz x3, 1b
-    //dmb ish
-    cbz x2, 1b
-    mov x4, xzr
-    str x4, [x2]
-    b 1b
-runCore1:
-    mov x1, 0x78000
-    mov sp, x1
-    bl drawPILogo
-1:
-    b 1b
+//	base of our GPIO structure
+	ldr x0, =GPIO_BASE
 
-// TODO: Later
-onMainCore:
+//	set the GPIO 21 function as output
+	ldr x1, =GPIO_21_OUTPUT
+	str x1, [x0, #GPFSEL2]
 
-drawPILogo:
-	//LOAD FRAME BUFFER ADDRESS
-	LDR X10, =FB
-	LDR X10, [X10]
-	
-	LDR X1, =PILOGO		//GET ADDRESS OF PILOGO DATA
-	LDR X2, =0x5DC00	//800x480 (ENTIRE SCREEN BUFFER)
-	MOV X4, XZR
-DRAWLOGO:
-	LDR W3, [X1, X4]
-	STR W3, [X10, X4]
-	ADD X4, X4, #04
-	SUBS X2, X2, #01
-	BGT DRAWLOGO
-	RET
+//	set counter
+	ldr x2, =0x800000
+
+loop:
+//	turn on the LED
+	ldr x1, =GPIOVAL
+	str x1, [x0, #GPFSET0]
+
+//	wait for some time delay
+	eor x10, x10, x10
+delay1:
+	add x10, x10, #1
+	cmp x10, x2
+	bne delay1
+
+	ldr x1, =GPIOVAL
+	str x1, [x0, #GPFCLR0]
+	eor x10, x10, x10
+delay2:
+	add x10, x10, #1
+	cmp x10, x2
+	bne delay2
+
+	b loop
