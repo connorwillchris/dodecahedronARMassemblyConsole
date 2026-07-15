ca15cde0e6de29b8f4ce439c2127a70e0317f0f4
treeman0013 <connorwillchris@gmail.com>
2026-07-14 18:55:10 -0600

    trying again 2...!

--- test/main.s
+++ test/main.s
@@ -1,6 +1,6 @@
 .global _start
 
-.equ GPIO_BASE, 0xfe200000 //0x3f200000
+.equ GPIO_BASE, 0x7e200000
 .equ GPFSEL2, 0x8
 .equ GPIO_21_OUTPUT, 0x8 // 1 << 3
 .equ GPFSET0, 0x1c
@@ -10,34 +10,34 @@
 //.text
 _start:
 //	base of our GPIO structure
-	ldr x0, =GPIO_BASE
+	ldr r0, =GPIO_BASE
 
 //	set the GPIO 21 function as output
-	ldr x1, =GPIO_21_OUTPUT
-	str x1, [x0, #GPFSEL2]
+	ldr r1, =GPIO_21_OUTPUT
+	str r1, [r0, #GPFSEL2]
 
 
 // 	NOTE: Keep at 64 bit version
-	ldr x2, =0x80000
+	ldr r2, =0x80000
 
 loop:
 //	turn on the LED
-	ldr x1, =GPIOVAL
-	str x1, [x0, #GPFSET0]
+	ldr r1, =GPIOVAL
+	str r1, [r0, #GPFSET0]
 
 //	wait for some time delay
-	eor x10, x10, x10
+	eor r10, r10, r10
 delay1:
-	add x10, x10, #1
-	cmp x10, x2
+	add r10, r10, #1
+	cmp r10, r2
 	bne delay1
 
-	ldr x1, =GPIOVAL
-	str x1, [x0, #GPFCLR0]
-	eor x10, x10, x10
+	ldr r1, =GPIOVAL
+	str r1, [r0, #GPFCLR0]
+	eor r10, r10, r10
 delay2:
-	add x10, x10, #1
-	cmp x10, x2
+	add r10, r10, #1
+	cmp r10, r2
 	bne delay2
 
 	b loop
